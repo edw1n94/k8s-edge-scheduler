0a0ede22532e90b70017158788a33309180ea220
nam <ngw42@naver.com>
2019-08-10 00:35:24 +0900

    first commit

---
+++ src/k8s-manager.py
@@ -0,0 +1,11 @@
+from kubernetes import client, config
+
+class k8s-manager(object):
+
+
+    def __init__(self):
+        v1 = client.CoreV1Api()
+        print("Listing pods with their IPs:")
+        ret = v1.list_pod_for_all_namespaces(watch=False)
+        for i in ret.items:
+            print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
